ed591a5210d42affce6cbf550acffff6cdead053
Itua Ijagbone <ituaijagbone@Ituas-MacBook-Pro.local>
2015-09-05 11:23:35 -0400

    Working on final pieces of iOS app

---
+++ Remote/ListPresentationViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Remote
+//
+//  Created by Itua Ijagbone on 9/5/15.
+//  Copyright (c) 2015 Itua Ijagbone. All rights reserved.
+//
+
+import UIKit
+
+class ListPresentationViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 215
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("cell") as! PresentationCell
+        
+        // TODO: Add cell model here
+        
+        return cell
+    }
+
+
+}
+
---
+++ Remote/PresentationCell.swift
@@ -0,0 +1,23 @@
+//
+//  PresentationCeil.swift
+//  Remote
+//
+//  Created by Itua Ijagbone on 9/5/15.
+//  Copyright (c) 2015 Itua Ijagbone. All rights reserved.
+//
+
+import UIKit
+
+class PresentationCell: UITableViewCell {
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
