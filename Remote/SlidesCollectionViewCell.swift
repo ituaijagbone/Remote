6401e3a90544994288b4078ae85626b64f7daf0d
Itua Ijagbone <ituaijagbone@users.noreply.github.com>
2015-11-10 00:01:10 -0600

    Adding Comments

--- Remote/SlidesCollectionViewCell.swift
+++ Remote/SlidesCollectionViewCell.swift
@@ -5,7 +5,7 @@
 //  Created by Itua Ijagbone on 9/5/15.
 //  Copyright (c) 2015 Itua Ijagbone. All rights reserved.
 //
-
+// Custom UITableViewCell for slide image
 import UIKit
 
 class SlidesCollectionViewCell: UICollectionViewCell {
